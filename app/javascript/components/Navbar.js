d6e8fb190b00182ae190813fbe5c189d321ff4c5
Katie Secrest <katie.secrest@gmail.com>
2021-06-09 21:11:31 -0600

    troubleshooting

--- app/javascript/components/Navbar.js
+++ app/javascript/components/Navbar.js
@@ -1,11 +1,12 @@
 import React from 'react'
+import Music_Icon from "./Images/Music_Icon.png" 
 
 const Navbar = () => {
   const cssBlock = 'bb-navbar';
   return (
     <div className={cssBlock} role='navigation'>
       <div className={`${cssBlock}_link ${cssBlock}_link--logo` }>
-        <img src="/images/Music_Icon.png" alt="music notes"/>
+        <img src={Music_Icon} alt="music notes" />
       </div>
       <a className={`${cssBlock}_link`} href='/'>Home</a>
       <a className={`${cssBlock}_link`} href='/about'>About</a>
